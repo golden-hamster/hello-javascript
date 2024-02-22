76d0e24518b09ac251555195c6480214c88268d6
golden-hamster <isack19999@gmail.com>
2024-02-23 00:12:31 +0900

    first commit

---
+++ src/main/java/com/example/Restfulapiboard/RestfulapiboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.Restfulapiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulapiboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulapiboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsboard/RestfulapiboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulapiboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
